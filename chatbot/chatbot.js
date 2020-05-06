a835bd528d74ae95db2f5d0775c89e467feaa334
Janhavi <janhaviprabhu27@gmail.com>
2020-05-06 16:55:05 +0530

    connect server side and dialogflow

---
+++ chatbot/chatbot.js
@@ -0,0 +1,42 @@
+'use strict'
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+
+const projectID = config.googleProjectID;
+
+const credentials = {
+	client_email: config.googleClientEmail,
+	private_key: config.googlePrivateKey
+};
+
+const sessionClient = new dialogflow.SessionsClient({projectID, credentials});
+
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
+module.exports = {
+	textQuery: async function(text, parameters = {}) {
+		let self = module.exports;
+		const request = {
+			session: sessionPath,
+			queryInput: {
+				text: {
+					text: text,
+					languageCode: config.dialogFlowSessionLanguageCode
+				},
+			},
+			queryParams: {
+				payload: {
+					date: parameters
+				}
+			}
+		};
+		let responses = await sessionClient.detectIntent(request);
+		responses = await self.handleAction(responses)
+		return responses;
+	},
+
+	handleAction: function(responses) {
+		return responses;
+	}
+
+}
